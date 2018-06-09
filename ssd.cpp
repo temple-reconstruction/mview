8f5ce664d33627e5707fd568234fcc410f7ab803
Zhuyue0324 <zhuyue0324@sina.com>
2018-06-09 21:26:34 +0200

    2 ssd functions for gray scale or RGB image

--- ssd.cpp
+++ ssd.cpp
@@ -2,7 +2,25 @@
 #include <stdio.h>
 #include <math.h> 
 
-float ssd_cost(ImageView left, ImageView right){
+//compute ssd for 2 gray scale images
+float ssd_cost_gray(ImageView left, ImageView right){
+    assert((left.height==right.height)&&("Two imageviews pass into ssd_cost don't have the same height"));
+    assert((left.width==right.width)&&("Two imageviews pass into ssd_cost don't have the same width"));
+    float cost=0.0;
+    for (int j=0;j<left.height;j++){
+        for (int i=0;j<left.width;i++){
+            cost=cost+pow((
+                left.image.Gray_pixel_values((left.left_column+i),(left.top_row+j))
+                -right.image.Gray_pixel_values((right.left_column+i),(right.top_row+j))
+            ),2);
+        }
+    }
+    return cost;
+    
+}
+
+//compute ssd for 2 RGB images
+float ssd_cost_RGB(ImageView left, ImageView right){
     assert((left.height==right.height)&&("Two imageviews pass into ssd_cost don't have the same height"));
     assert((left.width==right.width)&&("Two imageviews pass into ssd_cost don't have the same width"));
     float cost=0.0;
@@ -10,8 +28,8 @@ float ssd_cost(ImageView left, ImageView right){
         for (int i=0;j<left.width;i++){
             for (int k=0;k<3;k++){
                 cost=cost+pow((
-                    left.image.pixel_values(((left.top_row+j)*left.width+left.left_column+i),k)
-                    -right.image.pixel_values(((right.top_row+j)*right.width+right.left_column+i),k)
+                    left.image.RGB_pixel_values((left.left_column+i),(left.top_row+j))(k)
+                    -right.image.RGB_pixel_values((right.left_column+i),(right.top_row+j))(k)
                     ),2);
             } 
         }
@@ -19,3 +37,4 @@ float ssd_cost(ImageView left, ImageView right){
     return cost;
     
 }
+
