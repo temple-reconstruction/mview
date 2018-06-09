c3d53da612e4d3605c21a17937e24dd77e1ba200
Zhuyue0324 <zhuyue0324@sina.com>
2018-06-09 22:14:03 +0200

    can choose using face or not, turn dist to static

--- write_mesh.cpp
+++ write_mesh.cpp
@@ -6,11 +6,13 @@
 #define MINF -std::numeric_limits<float>::infinity()
 #endif
 
-float dist(Eigen::VectorXf a, Eigen::VectorXf b){
-    return sqrt(pow((a(0)-b(0)),2)+pow((a(1)-b(1)),2)+pow((a(2)-b(2)),2));
+#define USE_FACE  0
+
+static float dist(Eigen::Vector3f a, Eigen::Vector3f b) {
+    return (a - b).norm();
 }
 
-bool write_mesh(const std::string& filename, Pointcloud pointcloud)
+bool write_mesh(const std::string& filename, Pointcloud pointcloud, Pointcloud colour)
 {
 	float edgeThreshold = 0.001f;
 
@@ -19,15 +21,17 @@ bool write_mesh(const std::string& filename, Pointcloud pointcloud)
 
 	//number of faces
 	unsigned int nFaces = 0;
-	for (int i=0;i<nVertices-2;i++){
-		for (int j=i+1;j<nVertices-1;j++){
-            for (int k=j+1;k<nVertices;k++){
-                if ((pointcloud.points[i](0)!=MINF)&&(pointcloud.points[j](0)!=MINF)&&(pointcloud.points[k](0)!=MINF)){
-                    if ((dist(pointcloud.points[i],pointcloud.points[j])<edgeThreshold)&&(dist(pointcloud.points[i],pointcloud.points[k])<edgeThreshold)&&(dist(pointcloud.points[j],pointcloud.points[k])<edgeThreshold)){
-					    nFaces++;
-                    }
-			    }
-            }
+	if (USE_FACE){ 		
+		for (int i=0;i<nVertices-2;i++){
+			for (int j=i+1;j<nVertices-1;j++){
+            	for (int k=j+1;k<nVertices;k++){
+                	if ((pointcloud.points[i](0)!=MINF)&&(pointcloud.points[j](0)!=MINF)&&(pointcloud.points[k](0)!=MINF)){
+                    	if ((dist(pointcloud.points[i],pointcloud.points[j])<edgeThreshold)&&(dist(pointcloud.points[i],pointcloud.points[k])<edgeThreshold)&&(dist(pointcloud.points[j],pointcloud.points[k])<edgeThreshold)){
+					    	nFaces++;
+                    	}
+			    	}
+            	}
+			}
 		}
 	}
 
@@ -46,20 +50,22 @@ bool write_mesh(const std::string& filename, Pointcloud pointcloud)
 		}
 		else{
 			outFile << pointcloud.points[i](0) << " " << pointcloud.points[i](1) << " " << pointcloud.points[i](2) << " " <<
-			 (int)pointcloud.points[i](3) << " " << (int)pointcloud.points[i](4) << " " << (int)pointcloud.points[i](5) << " " << (int)pointcloud.points[i](6) << std::endl;
+			 (int)colour.points[i](0) << " " << (int)colour.points[i](1) << " " << (int)colour.points[i](2) << " 0" << std::endl;
 		}
 	}
 	
-	// TODO: save faces
-    for (int i=0;i<nVertices-2;i++){
-		for (int j=i+1;j<nVertices-1;j++){
-            for (int k=j+1;k<nVertices;k++){
-                if ((pointcloud.points[i](0)!=MINF)&&(pointcloud.points[j](0)!=MINF)&&(pointcloud.points[k](0)!=MINF)){
-                    if ((dist(pointcloud.points[i],pointcloud.points[j])<edgeThreshold)&&(dist(pointcloud.points[i],pointcloud.points[k])<edgeThreshold)&&(dist(pointcloud.points[j],pointcloud.points[k])<edgeThreshold)){
-					    outFile << "3 " << i << " " << j << " " << k << std::endl;
-                    }
-			    }
-            }
+	// save faces
+	if(USE_FACE){
+    	for (int i=0;i<nVertices-2;i++){
+			for (int j=i+1;j<nVertices-1;j++){
+            	for (int k=j+1;k<nVertices;k++){
+                	if ((pointcloud.points[i](0)!=MINF)&&(pointcloud.points[j](0)!=MINF)&&(pointcloud.points[k](0)!=MINF)){
+                    	if ((dist(pointcloud.points[i],pointcloud.points[j])<edgeThreshold)&&(dist(pointcloud.points[i],pointcloud.points[k])<edgeThreshold)&&(dist(pointcloud.points[j],pointcloud.points[k])<edgeThreshold)){
+					    	outFile << "3 " << i << " " << j << " " << k << std::endl;
+                    	}
+			    	}
+            	}
+			}
 		}
 	}
 
