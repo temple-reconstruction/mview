c6f2555b7e66dec18af95164908072ea1e1f3d53
HeroicKatora <andreas.molzer@gmx.de>
2018-06-14 10:27:39 +0200

    Adapt ssd to colour and gray image

--- match.cpp
+++ match.cpp
@@ -4,7 +4,9 @@ namespace /* local types */ {
 
 using Matches = std::vector<Correspondence>;
 using PixelCoordinates = Correspondence::PixelCoordinates;
-using BlockView = decltype(std::declval<const Eigen::MatrixXf>().block(0, 0, 0, 0)); 
+
+using GrayBlockView = decltype(std::declval<const GrayImage>().block(0, 0, 0, 0)); 
+using RgbBlockView = decltype(std::declval<const RgbImage>().block(0, 0, 0, 0)); 
 
 struct MinMatch { 
 	int colIndex; 
@@ -13,12 +15,18 @@ struct MinMatch {
 
 }
 
-static MinMatch find_min(BlockView pattern, const Eigen::MatrixXf& rhs, int row);
+static MinMatch find_min_gray(GrayImageView pattern, const GrayImage& rhs, int row);
+
 static Correspondence match_to_correspondence(int row, int col, MinMatch match);
+static float ssd_gray(GrayImageView left, GrayImageView right);
+static float ssd_rgb(RgbImageView left, RgbImageView right);
+
+static GrayBlockView gray_block(GrayImageView);
+static RgbBlockView rgb_block(RgbImageView);
 
 auto match(const Rectified& rectified) -> Matches {
-	const auto& pixel_left = rectified.pixel_left;
-	const auto& pixel_right = rectified.pixel_right;
+	const auto& pixel_left = rectified.pixel_left_gray;
+	const auto& pixel_right = rectified.pixel_right_gray;
 
 	assert(pixel_left.rows() == pixel_right.rows());
 
@@ -26,7 +34,8 @@ auto match(const Rectified& rectified) -> Matches {
 
 	for(int i = 0; i < pixel_left.rows(); i++) {
 		for(int j = 0; j < pixel_left.cols(); j++) {
-			const auto min = find_min(pixel_left.block(i, j, 1, 1), pixel_right, i);
+			const GrayImageView pattern { pixel_left, i, j, 1, 1 };
+			const auto min = find_min_gray(pattern, pixel_right, i);
 
 			matches.push_back(match_to_correspondence(i, j, min));
 		}
@@ -35,15 +44,13 @@ auto match(const Rectified& rectified) -> Matches {
 	return matches;
 }
 
-MinMatch find_min(BlockView pattern, const Eigen::MatrixXf& rhs, int row) {
+MinMatch find_min_gray(GrayImageView pattern, const GrayImage& rhs, int row) {
 	MinMatch best_match { 0, 0.0 };
 
 	for(int j = 0; j < rhs.cols(); j++) {
-		const BlockView compare = rhs.block(row, j, 1, 1);
+		const GrayImageView compare { rhs, row, j, 1, 1 };
 
-		assert(pattern.rows() == compare.rows() && pattern.cols() == compare.cols());
-
-		const auto cost = (pattern - compare).squaredNorm();
+		const auto cost = ssd_gray(pattern, compare);
 		if(cost < best_match.cost)
 			best_match = { j, cost };
 	}
@@ -57,3 +64,24 @@ Correspondence match_to_correspondence(int row, int col, MinMatch match) {
 
 	return { coords_left, coords_right, match.cost, {} };
 }
+
+float ssd_gray(GrayImageView ileft, GrayImageView iright) {
+	const GrayBlockView left = gray_block(ileft);
+	const GrayBlockView right = gray_block(iright);
+	return (left - right).squaredNorm();
+}
+
+float ssd_rgb(RgbImageView ileft, RgbImageView iright) {
+	const RgbBlockView left = rgb_block(ileft);
+	const RgbBlockView right = rgb_block(iright);
+	return (left - right).unaryExpr([](auto c){ return c.squaredNorm(); }).sum();
+}
+
+GrayBlockView gray_block(GrayImageView view) {
+	return view.image.block(view.top_row, view.left_column, view.height, view.width);
+}
+
+RgbBlockView rgb_block(RgbImageView view) {
+	return view.image.block(view.top_row, view.left_column, view.height, view.width);
+}
+
--- mview.h
+++ mview.h
@@ -3,6 +3,9 @@
 #include <ostream>
 #include <Eigen/Eigen>
 
+using GrayImage = Eigen::MatrixXf;
+using RgbImage = Eigen::Matrix<Eigen::Vector3f, Eigen::Dynamic, Eigen::Dynamic>;
+
 /// Read from the parameter file `<name>_par.txt`
 struct CameraParameter {
 	std::string filename;
@@ -12,15 +15,19 @@ struct CameraParameter {
 
 /// A possibly rectified image 
 struct Image {
-	Eigen::MatrixXf pixel_values;
+	GrayImage gray_pixels;
+	RgbImage rgb_pixels;
 	Eigen::Matrix3f intrinsics;
 	Eigen::Matrix4f extrinsics;
 };
 
 /// A pair of images rectified onto a single virtual camera plane with common baselines.
 struct Rectified {
-	Eigen::MatrixXf pixel_left;
-	Eigen::MatrixXf pixel_right;
+	GrayImage pixel_left_gray;
+	GrayImage pixel_right_gray;
+
+	RgbImage pixel_left_rgb;
+	RgbImage pixel_right_rgb;
 
 	float baseline_distance;
 	Eigen::Matrix4f extrinsics;
@@ -46,8 +53,13 @@ struct Correspondence {
 };
 
 /// A rectangular area of pixel in an image.
-struct ImageView {
-	const Image& image;
+struct GrayImageView {
+	const GrayImage& image;
+	int top_row, left_column, height, width;
+};
+
+struct RgbImageView {
+	const RgbImage& image;
 	int top_row, left_column, height, width;
 };
 
@@ -57,7 +69,8 @@ struct Pointcloud {
 };
 
 ///@author Yue
-float ssd_cost(ImageView left, ImageView right);
+float ssd_cost(GrayImageView left, GrayImageView right);
+float ssd_cost_rgb(RgbImageView left, RgbImageView right);
 
 ///@author Yu
 auto rectify(const Image& left, const Image& right) -> Rectified;
