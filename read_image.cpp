e1c9bbd45878b47c72fadbddf2f73e9c23f830f0
Andreas Molzer <andreas.molzer@gmx.de>
2018-06-22 09:39:16 +0200

    Reading rgb images

--- read_image.cpp
+++ read_image.cpp
@@ -1,39 +1,39 @@
 #include "mview.h"
 #include "FreeImage.h"
 
-GrayImage ReadImageFromFile(std::string);
+static std::pair<GrayImage, RgbImage> ReadImageFromFile(std::string);
 
-auto read_image(CameraParameter &cameraParameter) -> Image
+auto read_image(CameraParameter cameraParameter) -> Image
 {
   Image image;
   image.intrinsics = cameraParameter.intrinsics;
   image.extrinsics = cameraParameter.extrinsics;
-  image.rgb_pixels = ReadImageFromFile(cameraParameter.filename);
+  std::tie(image.gray_pixels, image.rgb_pixels) = ReadImageFromFile(cameraParameter.filename);
 
   return image;
 }
 
-GrayImage ReadImageFromFile(std::string &filename)
+std::pair<GrayImage, RgbImage> ReadImageFromFile(std::string filename)
 {
   FreeImage_Initialise();
 
   //image format
   FREE_IMAGE_FORMAT fif = FIF_UNKNOWN;
   //pointer to the image, once loaded
-  FIBITMAP *dib(0);
+  FIBITMAP *dib = nullptr;
 
   //check the file signature and deduce its format
   fif = FreeImage_GetFileType(filename.c_str(), 0);
   if (fif == FIF_UNKNOWN)
     fif = FreeImage_GetFIFFromFilename(filename.c_str());
   if (fif == FIF_UNKNOWN)
-    std::runtime_error("Data image format can not be determined");
+    throw std::runtime_error("Data image format can not be determined");
 
   //check that the plugin has reading capabilities and load the file
   if (FreeImage_FIFSupportsReading(fif))
     dib = FreeImage_Load(fif, filename.c_str());
   if (!dib)
-    std::runtime_error("Image could not be read");
+    throw std::runtime_error("Image could not be read");
 
   // Convert to RGBA float images
   FIBITMAP *hOldImage = dib;
@@ -41,39 +41,31 @@ GrayImage ReadImageFromFile(std::string &filename)
   FreeImage_Unload(hOldImage);
 
   //get the image width and height
-  w = FreeImage_GetWidth(dib);
-  h = FreeImage_GetHeight(dib);
-
-  // rescale to fit width and height
-  if (width != 0 && height != 0)
-  {
-    FIBITMAP *hOldImage = dib;
-    dib = FreeImage_Rescale(hOldImage, width, height, FILTER_CATMULLROM);
-    FreeImage_Unload(hOldImage);
-    w = width;
-    h = height;
-  }
+  int w = FreeImage_GetWidth(dib);
+  int h = FreeImage_GetHeight(dib);
 
   //retrieve the image data
-  BYTE *bits = FreeImage_GetBits(dib);
+  auto bits = FreeImage_GetBits(dib);
 
   //if this somehow one of these failed (they shouldn't), return failure
   if ((bits == 0) || (w == 0) || (h == 0))
-    std::runtime_error("Invalid image");
+    throw std::runtime_error("Invalid image");
 
-  nChannels = 4;
+  // Eigen stores column major by default, we prefer pixel samples in row major.
+  using PixelSampleMatrix = Eigen::Matrix<float, Eigen::Dynamic, 3, Eigen::RowMajor>;
+  
+  // Map the pixel values skipping the alpha value by a larger stride.
+  using PixelSamples = Eigen::Map<PixelSampleMatrix, 0, Eigen::Stride<4, 1>>;
+  PixelSamples data_vector { reinterpret_cast<float*>(bits), w*h, 3 };
 
-  // copy image data
-  float* data = new float[nChannels * w * h];
+  RgbImage rgb_target { w, h };
+  for(int i = 0; i < w*h; i++)
+    rgb_target(i%w, i/w) = data_vector.row(i);
 
-  // flip
-  for (int y = 0; y < (int)h; ++y)
-  {
-    memcpy(&(data[y * nChannels * w]), &bits[sizeof(float) * (h - 1 - y) * nChannels * w], sizeof(float) * nChannels * w);
-  }
+  GrayImage gray_target;
 
   //Free FreeImage's copy of the data
   FreeImage_Unload(dib);
 
-  return data;
+  return {gray_target, rgb_target};
 }
